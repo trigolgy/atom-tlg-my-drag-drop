fe173d579808a26d8ee02737ca2da3b8ae5c995a
trigolgy <tlg.kilinc@gmail.com>
2018-08-06 00:28:21 +0200

    atom package deneme
    atom package deneme. test kodlari...

---
+++ spec/tlg-my-drag-drop-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TlgMyDragDropView from '../lib/tlg-my-drag-drop-view';
+
+describe('TlgMyDragDropView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
